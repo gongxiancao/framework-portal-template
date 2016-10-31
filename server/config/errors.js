5d71cd7727f5215c872398da9d282c8576c245f4
GongXian Cao <gongxiancao@hotmail.com>
2016-10-31 17:07:47 +0800

    name framework to ofa

--- server/config/errors.js
+++ server/config/errors.js
@@ -1,1 +1,11 @@
-global.Errors = require('mlop-error');
+global.Errors = require('operational-error')({
+  FailedToGenerateTokenError: 'failed to generate token',
+  InternalError: 'internal error, contact us',
+  InvalidTokenError: 'invalid token',
+  PasswordNotMatchError: 'password not match',
+  RequirePasswordError: 'password required',
+  RequireTokenError: 'token required',
+  RequireUsernameError: 'username required',
+  TokenExpiredError: 'token expired',
+  UserNotExistError: 'user not exist'
+});
